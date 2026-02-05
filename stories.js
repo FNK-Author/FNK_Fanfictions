7f37f1030a5dc4544cc042c0c8f9e22ce168331c
FNK <fnk.author@gmail.com>
2026-02-05 01:28:36 +0100

    added stories.js

---
+++ stories.js
@@ -0,0 +1,215 @@
+
+    const stories = {
+      "Die Hexe in der Nacht (Harry Potter)": {
+        prefix: "dhidn_",
+        chapters: [
+          "Der Fang seines Lebens","Die Regelbrecher","Missetat begangen","Die Feier","Neugier",
+          "Das Geheimnis","Der Trank","Der Sturm","Ein Wunsch geht in Erfüllung","Bis wir uns irgendwann wiedersehen und zusammenwachsen",
+          "Geschichten in der Hütte","Die Nacht ist noch nicht vorbei","Apfel-Zimt","Hirsch und Pferd","Die Schuld",
+          "Pfefferminz Melodie","Eine kleine Ewigkeit","Eskalation - Teil 1","Eskalation - Teil 2","Träumerei",
+          "Tunichtgut","Erinnerungslücken","Unverzeihlich","Der Tiefpunkt","Felicity - Teil 1: Das Geheimnis",
+          "Felicity - Teil 2: Die Freundschaft","Felicity - Teil 3: Der Unfall","Felicity - Teil 4: Das Portrait","Ein offenes Ohr","Therapie",
+          "Solo","Der Hexentag - Teil 1","Der Hexentag - Teil 2","Der Hexentag - Teil 3","Der Hexentag - Teil 4"
+        ],
+        triggers: {
+          0: [
+            { line: 1, music: "yk0739" },
+            { line: 5, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k1_1.png", nsfw: true },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/b0401b1f4d98124a26be446520356034f23248ae/dhidn_1.png" },
+            { line: 1, video: "https://www.youtube.com/embed/uwJ5CX20q6I?si=FDKTg3V8E0f1oyO9" }
+          ],
+          1: [
+            { line: 1, music: "5bgsn9" },
+            { line: 49, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k2_1.png", nsfw: true }
+          ],
+          2: [
+            { line: 1, music: "h0ok1d" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k3_1.png", nsfw: true }
+          ],
+          3: [
+            { line: 1, music: "aikb5w" },
+            { line: 29, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k4_1.png", nsfw: true }
+          ],
+          4: [
+            { line: 1, music: "" },
+            { line: 15, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k5_1.png", nsfw: true }
+          ],
+          5: [
+            { line: 1, music: "" },
+            { line: 57, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k6_1.png", nsfw: true }
+          ],
+          6: [
+            { line: 1, music: "" },
+            { line: 17, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k7_1.png", nsfw: true }
+          ],
+          7: [
+            { line: 1, music: "" },
+            { line: 57, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k8_1.png", nsfw: true }
+          ],
+          8: [
+            { line: 1, music: "" },
+            { line: 14, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k9_1.png", nsfw: true }
+          ],
+          9: [
+            { line: 1, music: "" },
+            { line: 293, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k10_1.png", nsfw: true }
+          ],
+          10: [
+            { line: 1, music: "" },
+            { line: 138, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k11_1.png", nsfw: true }
+          ],
+          11: [
+            { line: 1, music: "" },
+            { line: 138, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k12_1.png", nsfw: true }
+          ],
+          12: [
+            { line: 1, music: "" },
+            { line: 159, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k13_1.png", nsfw: true }
+          ],
+          13: [
+            { line: 1, music: "" },
+            { line: 29, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k14_1.png", nsfw: true }
+          ],
+          14: [
+            { line: 1, music: "" },
+            { line: 21, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k15_1.png", nsfw: true }
+          ],
+          15: [
+            { line: 1, music: "" },
+            { line: 145, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k16_1.png", nsfw: true }
+          ],
+          16: [
+            { line: 1, music: "" },
+            { line: 232, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k17_1.png", nsfw: true }
+          ],
+          17: [
+            { line: 1, music: "" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k18_1.png", nsfw: true }
+          ],
+          18: [
+            { line: 1, music: "" },
+            { line: 141, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dhidn_k19_1.png", nsfw: true }
+          ],
+          19: [{ line: 1, music: "" }],
+          20: [{ line: 1, music: "" }],
+          21: [{ line: 1, music: "" }],
+          22: [{ line: 1, music: "" }],
+          23: [{ line: 1, music: "" }],
+          24: [{ line: 1, music: "" }],
+          25: [{ line: 1, music: "" }],
+          26: [{ line: 1, music: "" }],
+          27: [{ line: 1, music: "" }],
+          28: [{ line: 1, music: "" }],
+          29: [{ line: 1, music: "" }],
+          30: [{ line: 1, music: "" }],
+          31: [{ line: 1, music: "" }],
+          32: [{ line: 1, music: "" }],
+          33: [{ line: 1, music: "" }]
+        }
+      },
+      "SADMAX (Stranger Things)": {
+        prefix: "sadmax_",
+        chapters: ["Prolog","22. Oktober 1984"],
+        triggers: {
+          0: [
+            { line: 1, music: "e5jqa4" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/sadmax_1.png" },
+            { line: 1, video: "https://www.youtube.com/embed/FtMBI8l4lnI?si=zda9YfT8cq_hbF3h" }
+          ],
+          1: [
+            { line: 1, music: "e5jqa4" }
+          ]
+        }
+      },
+      "Gefangen (Star Wars)": {
+        prefix: "gefangen_",
+        chapters: ["Tag 1","Tag 2","Tag 3","Tag 4","Tag 5","Tag 6 - Morgendämmerung"],
+        triggers: {
+          0: [
+            { line: 1, music: "g8xtlu" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/gefangen_1.png" },
+            { line: 150, music: "z8pr7g" }
+          ],
+          1: [{ line: 1, music: "z8pr7g" }],
+          2: [{ line: 1, music: "z8pr7g" }],
+          3: [{ line: 1, music: "z8pr7g" }],
+          4: [{ line: 1, music: "z8pr7g" }],
+          5: [{ line: 1, music: "z8pr7g" }]
+        }
+      },
+      "Veilchen für Vi (Arcane)": {
+        prefix: "vfv_",
+        chapters: ["Veilchen für Vi"],
+        triggers: {
+          0: [
+            { line: 1, music: "rwvxfl" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/vfv_1.png" }
+          ]
+        }
+      },
+      "Erinnermich, der Harry Potter Adventskalender 2024": {
+        prefix: "erinnermich24_",
+        chapters: [
+          "Vorwort","Harry Potter","Severus Snape","Fred Weasley","George Weasley",
+          "Luna Lovegood","Draco Malfoy","Sirius Black","Remus Lupin","Bellatrix Lestrange",
+          "Lucius Malfoy","Ginny Weasley","Ron Weasley","Neville Longbottom","Albus Dumbledore",
+          "Viktor Krum","Molly Weasley","Percy Weasley","Lily Potter","James Potter",
+          "Nymphadora Tonks","Minerva McGonagall","Dobby, der freie Elf","Rubeus Hagrid","Hermine Granger",
+          "Nachwort"
+        ],
+        triggers: {
+          0: [
+            { line: 1, music: "rs01xb" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/erinnermich24_1.png" }
+          ],
+          1: [{ line: 1, music: "rs01xb" }],
+          2: [{ line: 1, music: "" }],
+          3: [{ line: 1, music: "" }],
+          4: [{ line: 1, music: "" }],
+          5: [{ line: 1, music: "" }],
+          6: [{ line: 1, music: "" }],
+          7: [{ line: 1, music: "" }],
+          8: [{ line: 1, music: "" }],
+          9: [{ line: 1, music: "" }],
+          10: [{ line: 1, music: "" }],
+          11: [{ line: 1, music: "" }],
+          12: [{ line: 1, music: "" }],
+          13: [{ line: 1, music: "" }],
+          14: [{ line: 1, music: "" }],
+          15: [{ line: 1, music: "" }],
+          16: [{ line: 1, music: "" }],
+          17: [{ line: 1, music: "" }],
+          18: [{ line: 1, music: "" }],
+          19: [{ line: 1, music: "" }],
+          20: [{ line: 1, music: "" }],
+          21: [{ line: 1, music: "" }],
+          22: [{ line: 1, music: "" }],
+          23: [{ line: 1, music: "" }],
+          24: [{ line: 1, music: "" }],
+          25: [{ line: 1, music: "rs01xb" }]
+        }
+      },
+      "化身的职责 - Die Pflicht eines Avatars": {
+        prefix: "dpea_",
+        chapters: ["Die Pflicht eines Avatars"],
+        triggers: {
+          0: [
+            { line: 1, music: "0dbynt" },
+            { line: 84, music: "58x5ab" },
+            { line: 122, music: "z7ex7j" },
+            { line: 196, music: "37fmcj" },
+            { line: 234, music: "xvak64" },
+            { line: 294, music: "hf5fxr" },
+            { line: 397, music: "7kyjgw" },
+            { line: 427, music: "0brp19" },
+            { line: 547, music: "rg0tzw" },
+            { line: 1, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dpea_1.png" },
+            { line: 62, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dpea_2.png", nsfw: true },
+            { line: 136, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dpea_3.png", nsfw: true },
+            { line: 297, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dpea_4.png", nsfw: true },
+            { line: 445, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dpea_5.png", nsfw: true },
+            { line: 555, image: "https://raw.githubusercontent.com/FNK-Author/FNK_Fanfictions/refs/heads/main/dpea_6.png", nsfw: true }
+          ]
+        }
+      }
+    };
